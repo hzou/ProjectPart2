d370957b72506b503417992fe6e757e0f3f6af7c
hzou <hzou@ualberta.ca>
2015-01-28 11:51:06 -0700

    Trial and error for Assignment 1

---
+++ ExpenseTracker/gen/jz/expensetracker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jz.expensetracker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ExpenseTracker/src/jz/expensetracker/Expense.java
@@ -0,0 +1,25 @@
+package jz.expensetracker;
+
+public class Expense {
+	private int amount;
+	private String description;
+	private int category;
+	
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Expense(int amount, String description) {
+		super();
+		this.amount = amount;
+		this.description = description;
+	}
+}
