4344c9d316d7f1d8c323623af32e50a80f7c1829
Han Luo <hluo2@cs.ualberta.ca>
2015-02-10 21:09:00 -0700

    License

---
+++ TeamProjectTest/src/ExpenseItemsListingTest.java
@@ -0,0 +1,12 @@
+import team14.teamproject.ClaimModel;
+import android.test.ActivityInstrumentationTestCase2;
+import junit.framework.TestCase;
+
+
+public class ExpenseItemsListingTest  extends ActivityInstrumentationTestCase2<AddExpenseActivity> {
+	private ClaimModel claim;
+	public void testListClaim(){
+		//Test if the list is empty
+	}
+	
+}
